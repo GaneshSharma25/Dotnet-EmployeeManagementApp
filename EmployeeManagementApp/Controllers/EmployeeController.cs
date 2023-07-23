36a7a3cc3d72847877f5d7dabe74c796d63bd72a
ganesh <ganesharts25@gmail.com>
2023-07-23 22:27:46 +0530

    add

--- EmployeeManagementApp/Controllers/EmployeeController.cs
+++ EmployeeManagementApp/Controllers/EmployeeController.cs
@@ -38,7 +38,8 @@ namespace EmployeeManagementApp.Controllers
             if (flag&& matchedEmployee != null)
             {
                 //return RedirectToAction("Welcome",matchedEmployee);
-                return View("Welcome", matchedEmployee);
+                ViewBag.ResultEmployee = matchedEmployee;
+                return View();
             }
             else
             {
@@ -68,9 +69,60 @@ namespace EmployeeManagementApp.Controllers
                 return View();
             }
         }
+
+        public IActionResult DeleteEmployee(int EmpId)
+        {
+            HrManager manager =new HrManager();
+            bool check = manager.DeleteEmployee(EmpId);
+            if(check)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Register");
+        }
         public IActionResult Welcome()
         {
+         
             return View();
+        }
+
+        public IActionResult UpdateEmployee(int empId)
+        {
+            HrManager manager = new HrManager();
+            Console.WriteLine(empId);
+            List<BOL.Employee> empList = manager.GetAllEmployees();
+            Employee matched = null;
+            empList.ForEach(delegate (Employee e)
+            {
+                if(e.EmpId == empId)
+                {
+                    matched = e;
+                    return;
+                }
+            });
+            if(matched != null)
+            {
+                return View("UpdateEmployee",matched);
+            }
+
+            // Employee emp1 = emp;
+            return View("UpdateEmployee","emp");
+        }
+        [HttpPost]
+        public IActionResult UpdateEmployee(int empid, string name, string email, string password, string location, string joindate, double salary)
+        {
+            Console.WriteLine("EmpId: "+empid +"Name: " + name +email +password);
+            Console.WriteLine("joindate: " + joindate);
+
+            Employee emp = new Employee(empid, name, email, password, location, DateOnly.Parse(joindate), salary);
+            HrManager manager = new HrManager();
+            bool check = manager.UpdateEmployee(emp);
+            if (check)
+            {
+               ViewBag.ResultEmployee = emp;
+                return View("Welcome");
+            }
+            return View("Login");
         }
     }
 }
